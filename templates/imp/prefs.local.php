208af7a3c8df203576962547f5386687b573ea0f
Christian Rodriguez <chrodriguez@gmail.com>
2019-09-30 23:35:25 -0300

    Set html compose to true and save attachements always

--- templates/imp/prefs.local.php
+++ templates/imp/prefs.local.php
@@ -7,4 +7,5 @@ $_prefs['newmail_notify']['value'] = 1;
 $_prefs['image_replacement']['value'] = 0;
 $_prefs['emoticons']['value'] = 1;
 $_prefs['remotemanagement']['suppress'] = true;
-
+$_prefs['compose_html']['value'] = 1;
+$_prefs['save_attachments']['value'] =  'always';
