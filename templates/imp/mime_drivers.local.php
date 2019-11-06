1c37e6c18e5aa8479bddf97f29029c28a92c4713
Christian Rodriguez <chrodriguez@gmail.com>
2019-11-06 14:51:44 -0300

    Disable pdf thumbnails

--- templates/imp/mime_drivers.local.php
+++ templates/imp/mime_drivers.local.php
@@ -1,2 +1,4 @@
 <?php
 $mime_drivers['html']['inline'] = strcasecmp(isset($_ENV['IMP_MIME_DRIVERS_HTML_INLINE'])?$_ENV['IMP_MIME_DRIVERS_HTML_INLINE']:'true','true') == 0;
+$mime_drivers['pdf']['thumbnails'] = false;
+
